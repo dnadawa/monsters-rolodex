181d3e267349ca011c8dcc52c2735e6c8e13615c
dnadawa <40896729+dnadawa@users.noreply.github.com>
2019-12-17 21:25:52 +0530

    Updates

---
+++ precache-manifest.035483841a36ed653643f30137104bb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325a3d7cddf58db1b3d521fcecbac7cf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45a46411f9b2ac60d3be",
+    "url": "/monsters-rolodex/static/css/main.2b582598.chunk.css"
+  },
+  {
+    "revision": "da13332bed53fd4d4dd4",
+    "url": "/monsters-rolodex/static/js/2.ace540cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.ace540cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45a46411f9b2ac60d3be",
+    "url": "/monsters-rolodex/static/js/main.3f095a55.chunk.js"
+  },
+  {
+    "revision": "d7bdf58622fd1c37a402",
+    "url": "/monsters-rolodex/static/js/runtime-main.56f88b66.js"
+  }
+]);
